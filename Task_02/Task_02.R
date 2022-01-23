bdb7543a852b66eb7c00157f63cc96494fb3aa59
Kam <klt0024@mix.wvu.edu>
2022-01-22 19:33:10 -0500

    Kam Tudor Task 02 upload b

--- Task_02/Task_02.R
+++ Task_02/Task_02.R
@@ -1,6 +1,6 @@
-#This reads in the file off the internet
+#Task02a
+#Part IV
 Data<-read.csv('http://jonsmitchell.com/data/beren.csv', stringsAsFactors = F)
-#This saves the data to wd to look at in Excel
 write.csv(Data, 'rawdata.csv', quote = F)
 
 length(Data)
@@ -18,11 +18,12 @@ Data[1:5,1:3]
 #How would you find the date of the 257th observation?
 Data[257, 1:3]
 
-#Which events are bottles
+#Task 02a Part V-i
 Feeds<-which(Data[,9]=='bottle')
 berenMilk<-Data[Feeds,]
 head(berenMilk)
-#How many rows? Each represents a bottle feed.
+
+#How many rows: 332. Each represents a bottle feed.
 nrow(berenMilk)
 
 Feeds<-which(Data[,'event']=='bottle')
@@ -33,6 +34,7 @@ head(Feeds)
 #both [] and the $ operator can be used to pull a variable from a dataset.
 #using the $ operator to call a column, and brackets for [row, column] can achieve the same result
 
+#Task 02a V-ii
 dayID<-apply(Data, 1, function(x) paste(x[1:3], collapse='-'))
 dateID<-sapply(dayID, as.Date, format="%Y-%m-%d", origin="2019-04-18")
 Data$age<-dateID-dateID[which(Data$event=='birth')]
@@ -48,3 +50,49 @@ head(beren2)
 head(beren3)
 
 write.csv(beren3, 'beren_new.csv', quote = F, row.names = FALSE)
+
+#Task 02b
+#Question 1: Hypothesis I requires data that isn't present, Hypothesis II is extremely vague
+
+#Task 02b Part IV
+
+Feeds<-which(beren3$event=="bottle")
+avgMilk<-mean(beren3$value[Feeds])
+
+#Question 2: avgMilk is in ounces
+#The value column contains the numbers associated with the units column, it is the number of ounces from each feed
+#the brackets are used to call a specific data point, Feeds is a vector of the beren3 data points that are bottle feeds
+#just calling beren3$value would give the value column from every event, it would mostly be NAs
+avgFeed<-tapply(beren3$value[Feeds], beren3$age[Feeds], mean) 
+varFeed<-tapply(beren3$value[Feeds], beren3$age[Feeds], var) 
+totalFeed<-tapply(beren3$value[Feeds], beren3$age[Feeds], sum)
+numFeed<-tapply(beren3$value[Feeds], beren3$age[Feeds], length) 
+
+avgFeed
+varFeed
+totalFeed
+numFeed
+
+#Task 02b partV
+
+cor(beren3$value[Feeds], beren3$age[Feeds])
+
+cor.test(beren3$value[Feeds],beren3$age[Feeds])
+
+berenCor<-cor.test(beren3$value[Feeds], beren3$age[Feeds])
+summary(berenCor)
+
+berenANOVA<-aov(beren3$value[Feeds] ~ beren3$caregiver[Feeds])
+berenANOVA
+boxplot(beren3$value[Feeds] ~ beren3$caregiver[Feeds], 
+  xlab= "who gave the bottle", ylab= "amount of milk consumed (oz")
+
+#Task 02b Part VI
+?par
+par(las=1, mar=c(5,5,1,1), mgp=c(2,0.5,0), tck=-0.01)
+
+plot(as.numeric(names(totalFeed)), 
+     totalFeed, type="b", pch=16, 
+     xlab="age in days", ylab="ounces of milk")
+abline(h=mean(totalFeed), lty=2, col="red")     
+dev.off()
